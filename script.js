9fd669ba2a5694e00ddfcfd857199d468b6cad1b
VP17-1752046 <luongtin8@gmail.com>
2024-05-20 22:50:00 +0700

    create header 60%

---
+++ script.js
@@ -0,0 +1,26 @@
+function showMenuMobi() {
+  const hamburgerButton = document.querySelector(".hamburger-button");
+  const mobiMenu = document.querySelector(".header__mobi");
+  const closeButton = document.querySelector(".close-button");
+
+  hamburgerButton.addEventListener("click", function () {
+    if (mobiMenu.style.display === "block") {
+      mobiMenu.style.animation = "closeMenuMobi 0.3s forwards";
+      setTimeout(() => {
+        mobiMenu.style.display = "none";
+      }, 300); // Match the duration of the slideOut animation
+    } else {
+      mobiMenu.style.display = "block";
+      mobiMenu.style.animation = "showMenuMobi 0.3s forwards";
+    }
+  });
+
+  closeButton.addEventListener("click", function () {
+    mobiMenu.style.animation = "closeMenuMobi 0.3s forwards";
+    setTimeout(() => {
+      mobiMenu.style.display = "none";
+    }, 300); // Match the duration of the slideOut animation
+  });
+}
+
+showMenuMobi();
