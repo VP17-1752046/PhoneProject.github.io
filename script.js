8825f5bafbdab819766841e4326b2fb7019c16ef
VP17-1752046 <luongtin8@gmail.com>
2024-05-23 21:17:49 +0700

    create function render favorite product

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showMenuMobi() {
       mobiMenu.style.animation = "closeMenuMobi 0.3s forwards";
       setTimeout(() => {
         mobiMenu.style.display = "none";
-      }, 300); // Match the duration of the slideOut animation
+      }, 300);
     } else {
       mobiMenu.style.display = "block";
       mobiMenu.style.animation = "showMenuMobi 0.3s forwards";
@@ -19,12 +19,13 @@ function showMenuMobi() {
     mobiMenu.style.animation = "closeMenuMobi 0.3s forwards";
     setTimeout(() => {
       mobiMenu.style.display = "none";
-    }, 300); // Match the duration of the slideOut animation
+    }, 300);
   });
 }
 
 showMenuMobi();
 
+// Slide banner
 document.addEventListener("DOMContentLoaded", function () {
   let currentIndex = 0;
   const slides = document.querySelectorAll(".banner__slide-list");
@@ -51,3 +52,53 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setInterval(nextSlide, 5000);
 });
+
+// Render favorite product
+const data = [
+  {
+    id: 1,
+    image: "./assets/favorite1.png",
+    name: "iPhone 14 Pro Max 128GB",
+    promotional: "27.900.000đ",
+    cost: "28.900.000đ",
+  },
+  {
+    id: 2,
+    image: "./assets/favorite2.png",
+    name: "iPhone 14 Pro Max 256GB",
+    promotional: "28.900.000đ",
+    cost: "29.500.000đ",
+  },
+  {
+    id: 3,
+    image: "./assets/favorite3.png",
+    name: "iPhone 15 Pro Max 256GB",
+    promotional: "29.900.000đ",
+    cost: "32.900.000đ",
+  },
+  {
+    id: 4,
+    image: "./assets/favorite4.png",
+    name: "iPhone 15 Pro Max 512GB",
+    promotional: "32.500.000đ",
+    cost: "36.500.000đ",
+  },
+];
+
+const favoriteProductList = document.querySelector(".favorite-product-content");
+
+for (let product of data) {
+  const productElement = document.createElement("div");
+  productElement.classList.add("favorite-product-list");
+
+  productElement.innerHTML = `<a href="#" class="product-img"
+            ><img src="${product.image}" alt="favorite1"
+          /></a>
+          <a href="#" class="product-name">${product.name}</a>
+          <div class="product-price">
+            <span class="promotional-price">${product.promotional}</span>
+            <span class="cost-price">${product.cost}</span>
+          </div>`;
+
+  favoriteProductList.appendChild(productElement);
+}
